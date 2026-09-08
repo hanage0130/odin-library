7aa51bf44ee9667ec7a7b4826d199cc4e86d8b17
hanage0130 <60529832+hanage0130@users.noreply.github.com>
2026-09-08 15:47:58 +0900

    2026/09/08

--- script.js
+++ script.js
@@ -1,28 +1,77 @@
 const myLibrary = [];
 
-function Book(author, title, pages, read, id) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = read;
-    this.id = id;
+function Book(title, author, pages, read, id) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = id;
 }
 
-function addBookToLibrary(author, title, pages, read) {
-    const id = crypto.randomUUID();
-    const book = new Book(author, title, pages, read, id);
-    myLibrary.push(book);
-    console.log(book, myLibrary);
-    const lib = document.getElementById('library');
-    lib.insertAdjacentHTML('beforeend', `<tbody><tr><th>${author}</th><th>${title}</th><th>${pages}</th><th>${read}</th><th>${id}</th></tr></tbody>`)
+Book.prototype.changeReadState = function() {
+  if (this.read) {
+    this.read = false;
+  } else {
+    this.read = true;
+  }
+};
 
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read, crypto.randomUUID()));
 }
 
-document.getElementById('btn').addEventListener('click', function() {
-    let author = document.getElementById('author').value;
-    let title = document.getElementById('title').value;
-    let pages = document.getElementById('pages').value;
-    let read = document.getElementById('read').checked;
-    console.log(author, title, pages, read);
-    addBookToLibrary(author, title, pages, read);
+console.log(myLibrary);
+
+function displayData() {
+  let display = document.querySelector(".display");
+  display.innerHTML = "<tr><th>Title</th><th>Author</th><th>Pages</th><th>Read</th><th>ID</th><th>Delete</th><th>Change read state</tr>";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    display.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.read}</td><td>${book.id}</td><td><button id="a${book.id}">delete</button></td><td><button id="b${book.id}">change read state</button></td></tr>`;
+  }
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let deleteButton = document.querySelector("#a" + book.id);
+    deleteButton.addEventListener("click", () => {
+      for (let j = 0; j < myLibrary.length; j++) {
+        if (myLibrary[j].id === book.id) {
+          myLibrary.splice(j, 1);
+        }
+      }
+      displayData();
+    });
+    let changeReadStateButton = document.querySelector("#b" + book.id);
+    changeReadStateButton.addEventListener("click", () => {
+      book.changeReadState();
+      displayData();
+    });
+  }
+}
+
+let submitButton = document.querySelector(".submit-button");
+
+submitButton.addEventListener("click", (e) => {
+  let inputTitle = document.querySelector(".input-title");
+  let inputAuthor = document.querySelector(".input-author");
+  let inputPages = document.querySelector(".input-pages");
+  let inputRead = document.querySelector(".input-read");
+  addBookToLibrary(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.checked);
+  displayData();
+  inputTitle.value = "";
+  inputAuthor.value = "";
+  inputPages.value = "";
+  inputRead.checked = false;
+  e.preventDefault();
 });
+
+let newBookButton = document.querySelector(".new-book-button");
+newBookButton.addEventListener("click", () => {
+  let newBookForm = document.querySelector(".new-book-form");
+  if (newBookForm.style.display === "none") {
+    newBookForm.style.display = "block";
+  } else {
+    newBookForm.style.display = "none";
+  }
+});
+
+displayData();
